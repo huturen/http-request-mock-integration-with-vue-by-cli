53a9bafe56d31e0a23183e97b343deaf7fbd5eb1
hushuilong <364346488@qq.com>
2022-04-03 15:08:51 +0800

    chore: add proxy.js & remote.js

---
+++ mock/samples/proxy.js
@@ -0,0 +1,11 @@
+/**
+ * @url https://jsonplaceholder.typicode.com/proxy-mode
+ * @proxy
+ * @header content-type: application/json
+ * @method get
+ */
+module.exports = {
+  msg: 'Proxy to local proxy server.',
+  nodejs: process.version
+};
+
