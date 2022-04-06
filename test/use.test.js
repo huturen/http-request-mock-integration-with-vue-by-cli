d15130c4a2e477e095cda67c93e4e47bfe34af7b
hushuilong <364346488@qq.com>
2022-04-06 20:44:44 +0800

    chore: add use.test.js

--- test/use.test.js
+++ test/use.test.js
@@ -1,14 +1,13 @@
 /* global it, expect */
+
 import axios from 'axios';
 import xhrAdapter from 'axios/lib/adapters/xhr';
 import HttpRequestMock from 'http-request-mock';
-
-axios.defaults.adapter = xhrAdapter;
 const mocker = HttpRequestMock.setupForUnitTest('xhr');
 
-mocker.use('../mock/samples/dynamic.js');
-
-it('should match object`', async () => {
+axios.defaults.adapter = xhrAdapter;
+it('use', async () => {
+  mocker.use('../mock/samples/dynamic.js');
   const res = await axios.get('https://jsonplaceholder.typicode.com/dynamic');
-  expect(res.data).toMatchObject({msg: 'ok'});
+  expect(res.data.msg).toBe('ok');
 });
